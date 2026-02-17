423b6ec9fa0830105f5018238b9fd3bc83433d75
Akmal Aldahdooh <a.aldahdouh@madfu.com.sa>
2026-02-17 13:18:14 +0300

    update

---
+++ src/edu.aramco.aspnet.api/Middlware/LoggingRequestMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace edu.aramco.aspnet.api.Middlware;
+
+public class LoggingRequestMiddleware(RequestDelegate next, 
+    ILogger<LoggingRequestMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        EventLog.WriteEntry("SMS", $"Request Coming at {DateTime.Now}", EventLogEntryType.Information, 5000);
+
+        //logger.LogError("New request is coming");
+        //logger.LogWarning("New request is coming");
+        //logger.LogTrace("New request is coming");
+        //logger.LogInformation("New request is coming");
+
+
+        var stopwatch = Stopwatch.StartNew();
+        await next(context);
+        var elapsedTime = stopwatch.ElapsedMilliseconds;
+
+        //EventLog.WriteEntry("Aramco", $"Request has been executed in {DateTime.Now}", EventLogEntryType.Information, 101);
+        EventLog.WriteEntry("Aramco", $"Request Ended at {DateTime.Now}", EventLogEntryType.Information, 5001);
+    }
+}
